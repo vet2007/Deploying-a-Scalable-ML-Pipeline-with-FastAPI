43e78a6603eb529111a82898bb67f4d98afd4517
VJo <vet2007@yahoo.com>
2024-12-18 23:02:40 -0600

    Completed Step 4: API

--- main.py
+++ main.py
@@ -26,25 +26,27 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+project_path = "/home/factsnotfeelings/Deploying-a-Scalable-ML-Pipeline-with-FastAPI-1"
+
+path = os.path.join(project_path, "model", "encoder.pkl") # TODO: enter the path for the saved encoder 
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = os.path.join(project_path, "model", "model.pkl") # TODO: enter the path for the saved model 
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI() # your code here
 
 # TODO: create a GET on the root giving a welcome message
-@app.get("/")
+@app.get("/greeting")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    return {"greeting":"Greetings!!"}
+    
 
 
 # TODO: create a POST on a different path that does model inference
-@app.post("/data/")
+@app.post("/inference/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
     data_dict = data.dict()
@@ -65,10 +67,12 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
+        data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
         # use training = False
         # do not need to pass lb as input
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference =model.predict(data_processed)  # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
